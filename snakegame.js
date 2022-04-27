362babf74e62b2468912c00d65bc26b586d89064
kbuczek <krzysztofbuczek@live.com>
2022-04-27 13:25:44 +0200

    render

--- snakegame.js
+++ snakegame.js
@@ -7,6 +7,8 @@ let SNAKE_DIRECTION = "E";
 let cells = new Map();
 let interval;
 
+let CELL_KEY = "0-0";
+
 function toKey(row, col) {
   return row + "-" + col;
 }
@@ -23,13 +25,20 @@ function initializeGameBoard() {
       cell.style.width = CELL_SIZE + "px";
       cell.style.height = CELL_SIZE + "px";
       cell.style.border = "1px solid white";
-      cells.set;
+      let key = toKey(i, j);
+      cells.set(key, cell);
       canvas.appendChild(cell);
     }
   }
 }
 
-function renderFrame() {}
+function renderFrame() {
+  let [row, col] = fromKey(CELL_KEY);
+  const cell = cells.get(CELL_KEY);
+  cell.style.backgroundColor = "white";
+  col++;
+  CELL_KEY = toKey(row, col);
+}
 
 function setSnakeDirection() {}
 
