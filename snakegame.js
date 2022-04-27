72baa7b996d9b0fe25a6cd464763da617310cb4c
kbuczek <krzysztofbuczek@live.com>
2022-04-27 15:15:10 +0200

    moving snake

--- snakegame.js
+++ snakegame.js
@@ -1,13 +1,16 @@
 let canvas = document.getElementById("canvas");
+let score = document.getElementById("score");
 
 const ROWS = 20;
 const COLUMNS = 20;
-const CELL_SIZE = 24;
-let SNAKE_DIRECTION = "E";
+const CELL_SIZE = 25;
+
 let cells = new Map();
+let nonEmptyKeys = new Set();
 let interval;
 
-let CELL_KEY = "0-0";
+let SNAKE_HEAD = "10-10";
+let SNAKE_DIRECTION = "R";
 
 function toKey(row, col) {
   return row + "-" + col;
@@ -24,7 +27,6 @@ function initializeGameBoard() {
       cell.style.float = "left";
       cell.style.width = CELL_SIZE + "px";
       cell.style.height = CELL_SIZE + "px";
-      cell.style.border = "1px solid white";
       let key = toKey(i, j);
       cells.set(key, cell);
       canvas.appendChild(cell);
@@ -32,19 +34,71 @@ function initializeGameBoard() {
   }
 }
 
+function generateFood() {}
+
 function renderFrame() {
-  let [row, col] = fromKey(CELL_KEY);
-  const cell = cells.get(CELL_KEY);
-  cell.style.backgroundColor = "white";
-  col++;
-  CELL_KEY = toKey(row, col);
+  let [row, col] = fromKey(SNAKE_HEAD);
+  const cell = cells.get(SNAKE_HEAD);
+  cell.style.backgroundColor = "Chartreuse";
+  cell.style.borderRadius = "7px";
+  cell.style.border = "0.5px solid black";
+  [row, col] = moveSnakeInDirection(row, col);
+  console.log(row, col);
+  SNAKE_HEAD = toKey(row, col);
 }
 
-function setSnakeDirection() {}
+function moveSnakeInDirection(row, col) {
+  switch (SNAKE_DIRECTION) {
+    case "up":
+      row--;
+      break;
+    case "right":
+      col++;
+      break;
+    case "down":
+      row++;
+      break;
+    case "left":
+      col--;
+      break;
+  }
+  return [row, col];
+}
+
+window.addEventListener("keydown", (e) => {
+  e.preventDefault();
+  switch (e.key) {
+    case "ArrowUp":
+    case "w":
+    case "W":
+      SNAKE_DIRECTION = "up";
+      break;
+    case "ArrowRight":
+    case "d":
+    case "D":
+      SNAKE_DIRECTION = "right";
+      break;
+    case "ArrowDown":
+    case "s":
+    case "S":
+      SNAKE_DIRECTION = "down";
+      break;
+    case "ArrowLeft":
+    case "a":
+    case "A":
+      SNAKE_DIRECTION = "left";
+      break;
+  }
+});
 
 function startGame() {
+  initializeGameBoard();
+  score.textContent = "0";
   interval = setInterval(renderFrame, 300);
 }
 
-initializeGameBoard();
+function endGame() {
+  clearInterval(interval);
+}
+
 startGame();
